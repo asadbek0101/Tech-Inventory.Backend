19d62e2a01567e1069c3a753017a4d55b74075fc
Asadbek <asadbekrejabboyev0101@gmail.com>
2024-05-16 12:51:09 +0500

    some changes

--- Tech-Inventory.WebApi/Controllers/PdfController.cs
+++ Tech-Inventory.WebApi/Controllers/PdfController.cs
@@ -1,38 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PuppeteerSharp;
-using System.Text.Json;
-using Tech_Inventory.Application.Features.PdfFeature.ObyektReport;
-
 
 namespace Tech_Inventory.WebApi.Controllers;
 
-public class PdfController : BaseController
+public class PdfController : ControllerBase
 {
     [HttpGet("GetObyektReport")]
-    public async Task<IActionResult> GeneratePdf(int Id)
+    public async Task<IActionResult> GeneratePdf()
     {
-        var obyektReport = await Mediator.Send(new ObyektReportRequest { Id = Id });
 
         var options = new LaunchOptions
         {
             Headless = true,
         };
 
-        var browserFetcher = new BrowserFetcher();
-        await browserFetcher.DownloadAsync();
-
         using var browser = await Puppeteer.LaunchAsync(options);
         using var page = await browser.NewPageAsync();
-        using var memoryStream = new MemoryStream();
 
-        var url = Url.ActionLink("Index", "PdfGenerate", new { model = JsonSerializer.Serialize(obyektReport) });
+        var htmlContent = "";
 
-        await page.GoToAsync(url);
+        htmlContent += "<h1 style='color: red; font-family: Arial, Helvetica, sans-serif; text-align: center;'> Asadbek Rejabboyev </h1>";
+
+        await page.SetContentAsync(htmlContent);
 
         var pdfStream = await page.PdfDataAsync();
 
         return File(pdfStream, "application/pdf", "ObyektReport.pdf");
     }
+
 }
 
 
